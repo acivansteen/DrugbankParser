874be4cac139f32d150d5d1e5307a59d8b987dfb
acivansteen <bram_vansteen@hotmail.com>
2015-05-27 13:44:44 +0200

    Drugbankparser init
    finally the right one initialised

---
+++ src/msk/drugbank4/Edge.java
@@ -0,0 +1,45 @@
+package msk.drugbank4;
+
+public class Edge {
+	private String from;
+	private String to;
+	
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		if (from == null) {
+			if (other.from != null)
+				return false;
+		} else if (!from.equals(other.from))
+			return false;
+		if (to == null) {
+			if (other.to != null)
+				return false;
+		} else if (!to.equals(other.to))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
